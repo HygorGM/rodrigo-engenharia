e6a8f3b064d80723f097a25cda8f09216c048bd3
Hygor Gonçalves Marques <127366511+HygorGM@users.noreply.github.com>
2025-10-31 13:26:26 -0300

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -28,13 +28,6 @@ useSeoMeta({
 <template>
   <UApp>
     <UHeader>
-      <template #left>
-        <NuxtLink to="/">
-          <AppLogo class="w-auto h-6 shrink-0" />
-        </NuxtLink>
-
-        <TemplateMenu />
-      </template>
 
       <template #right>
         <UColorModeButton />
