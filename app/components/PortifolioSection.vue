eb521acdb9fa92d42041a7b42911b38ae61bebc7
Hygor Gonçalves Marques <127366511+HygorGM@users.noreply.github.com>
2025-10-31 14:02:58 -0300

    correção bug

--- app/components/PortifolioSection.vue
+++ app/components/PortifolioSection.vue
@@ -51,7 +51,7 @@
         class="flex items-center justify-center bg-white p-4 rounded-xl shadow-md hover:shadow-xl transition-transform transform hover:-translate-y-2 duration-300"
       >
         <img
-          src="/imagens/correios.jpeg"
+          src="/imagens/correios.png"
           alt="Logo empresa 4"
           class="object-contain max-h-16"
         >
